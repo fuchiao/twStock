839dcbcec44435493c94acadcc0b00fe380f5dc5
zack.chang <zector1030@gmail.com>
2015-05-06 09:32:09 +0000

    show 0050 chart, issue: cannot show two chart at same time

--- server.js
+++ server.js
@@ -29,6 +29,21 @@ app.get('/TWII', function(req, res) {
     });
     console.log("visit TWII.csv");
 });
+app.get('/0050', function(req, res) {
+    csv.parse(fs.readFileSync('./0050.csv'), function(err, data) {
+        var headers = data[0];
+        data.shift();
+        var result = [];
+        for (var i = 0; i < data.length; i++) {
+            result.push({});
+            for (var j = 0; j < headers.length; j++) {
+                result[result.length-1][headers[j]] = data[i][j];
+            }
+        }
+        res.send(result);
+    });
+    console.log("visit 0050.csv");
+});
 var server = app.listen(1337, function () {
     var host = server.address().address
     var port = server.address().port
